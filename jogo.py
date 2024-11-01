15b5c1a9e8b3b9d652ec63c1a7ca425aefc974d5
Matheus496 <matheusc3@al.insper.edu.br>
2024-10-31 21:22:39 -0300

    movimento_player_att

--- jogo.py
+++ jogo.py
@@ -5,14 +5,15 @@ pygame.init()
 
 
 window = pygame.display.set_mode((LARG,ALT))
-pygame.display.set_caption('Titulo')
+pygame.display.set_caption('Máfia 5')
 rect = pygame.Rect
 # blocos para haver colisão
-blocos_chao = [rect(0,440, 335, 10),rect(195,590,150,10),rect(94,729,330,10),rect(445,630,420,10)] 
-blocos_par = [rect(875,530,10,100)]
+blocos_horizont = [rect(0,440, 335, 10),rect(195,590,150,10),rect(94,729,360,10),rect(445,630,430,10),rect(880,530,210,10),rect(1090,630,300,10),rect(1350,530,60,10),rect(1405,440,350,10),rect(1750,490,340,10),rect(2090,630,200,10)] 
+blocos_vert = [rect(440,650,10,100),rect(875,530,10,100),rect(1090,530,10,100),rect(1350,530,10,100),rect(1400,445,10,100),rect(1750,440,10,60),rect(2090,500,10,150),rect(2280,630,10,200)]
+# carregar assets
 assets = load_assets()
 
-
+player_rect = assets['personagem'].get_rect()
 larg_fundo = assets['fundo'].get_width()
 alt_fundo = assets['fundo'].get_height()
 
@@ -20,42 +21,55 @@ game = True
 camera_x = 0
 camera_y = 0
 p_speedx = 0
+p_speedy = 0
+player_x = 200
+player_y = 440
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
         if event.type == pygame.MOUSEMOTION:
             print(event)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:  
-        camera_x += 3
-    if keys[pygame.K_d]:  
-        camera_x -= 3
-    if keys[pygame.K_w]:
-        camera_y += 5
-    if keys[pygame.K_s]:
-        camera_y -= 5  
-    p_speedx = 0
-    if LARG>camera_x > 0  :
-        camera_x = 0
-        p_speedx += 3
-    if camera_x < -1900:
-        camera_x += 3
-        p_speedx += 3
-        
-    if camera_y < -280:
-        camera_y +=5
+        if event.type == pygame.KEYDOWN:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_d:
+                p_speedx += 4
+            if event.key == pygame.K_a:
+                p_speedx -= 4
+            if event.key == pygame.K_w:
+                p_speedy -= 4
+            if event.key == pygame.K_s:
+                p_speedy += 4
+        # Verifica se soltou alguma tecla.
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_d:
+                p_speedx -= 4
+            if event.key == pygame.K_a:
+                p_speedx += 4
+            if event.key == pygame.K_w:
+                p_speedy += 4
+            if event.key == pygame.K_s:
+                p_speedy -= 4
+    
+    
+    
+    
+    player_x += p_speedx
+    player_y += p_speedy
 
-    if camera_y > 0:
-        camera_y = 0
+    camera_x = player_x - LARG // 2
+    camera_y = player_y - ALT // 2
+    
     
     window.fill((255, 255, 255))  
-    window.blit(assets['fundo'],(camera_x,camera_y))
-    window.blit(assets['personagem'],(0 + p_speedx,ALT/2))
-    for bloco in blocos_chao:
-        pygame.draw.rect(window, (0, 255, 0), bloco.move(camera_x,camera_y))
-    for bloco in blocos_par:
-        pygame.draw.rect(window, (0, 255, 0), bloco.move(camera_x,camera_y))
+    window.blit(assets['fundo'],(-camera_x,-camera_y)) # atualiza o fundo
+    window.blit(assets['personagem'],(LARG // 2, ALT // 2)) # atualiza o jogador
+    for bloco in blocos_horizont:
+        pygame.draw.rect(window, (0, 255, 0), bloco.move(-camera_x,-camera_y))
+    for bloco in blocos_vert:
+        pygame.draw.rect(window, (0, 255, 0), bloco.move(-camera_x,-camera_y))
+
     pygame.display.flip()  # Atualiza a tela
     pygame.time.Clock().tick(FPS) 
 
